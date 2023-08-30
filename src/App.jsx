ae69bdf7bf02d9a6b66fe3a8c0461c1035931cde
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-30 08:56:41 +0200

    Add DoctorsList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Doctor from './components/doctors/DoctorList';      
-import AddDoctor from './components/doctors/AddDoctor';
-import Navigation from './components/Navigation'; // Import the Navigation component
+// import AddDoctor from './components/doctors/AddDoctor';
+import Navigation from './components/Navigation'; 
 
 const App = () => (
   <BrowserRouter>
     <Navigation />
     <Routes>
       <Route path="/" element={<Doctor />} />
-      <Route path="/addDoctor" element={<AddDoctor />} />
+      {/* <Route path="/addDoctor" element={<AddDoctor />} /> */}
     </Routes>
 
   </BrowserRouter>
