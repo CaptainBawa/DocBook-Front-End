abb0633f8304d559336d47f6df70d9d6cc24b93e
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-30 15:35:36 +0200

    Fix linters

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import "./App.css";
-import { BrowserRouter} from "react-router-dom";
-import Layout from "./components/layout";
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Layout from './components/layout';
 
 const App = () => (
   <BrowserRouter>
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,22 +1,20 @@
-import React from "react";
-import DoctorList from "./doctors/DoctorList";
-import Navigation from "./Navigation";
-import { Route, Routes } from "react-router-dom";
-import "../App.css";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import DoctorList from './doctors/DoctorList';
+import Navigation from './Navigation';
+import '../App.css';
 
-const Layout = () => {
-  return (
-    <div className="flex bg-white">
-      <div className="w-2/12">
-        <Navigation />
-      </div>
-      <div className="w-10/12 flex items-center">
-        <Routes>
-          <Route path="/" element={<DoctorList />} />
-        </Routes>
-      </div>
+const Layout = () => (
+  <div className="flex bg-white">
+    <div className="w-2/12">
+      <Navigation />
     </div>
-  );
-};
+    <div className="w-10/12 flex items-center">
+      <Routes>
+        <Route path="/" element={<DoctorList />} />
+      </Routes>
+    </div>
+  </div>
+);
 
 export default Layout;
