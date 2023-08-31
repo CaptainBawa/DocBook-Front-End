42fbed9d40bace06fbf435c4d43901087e1982d5
tomdan-ai <tomudoh258@gmail.com>
2023-08-31 17:25:43 +0100

    Make each doctor cards clickable link

--- src/components/doctors/DoctorList.jsx
+++ src/components/doctors/DoctorList.jsx
@@ -1,7 +1,61 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+// // src/components/doctors/DoctorList.jsx
+// import React, { useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { Link } from 'react-router-dom';
+// import {
+//   fetchDoctors,
+//   selectDoctors,
+//   selectDoctorsStatus,
+//   selectDoctorsError,
+// } from '../../redux/doctorsSlice';
+
+// function DoctorList() {
+//   const dispatch = useDispatch();
+//   const doctors = useSelector(selectDoctors);
+//   const status = useSelector(selectDoctorsStatus);
+//   const error = useSelector(selectDoctorsError);
+
+//   useEffect(() => {
+//     dispatch(fetchDoctors());
+//   }, [dispatch]);
+
+//   return (
+//     <div className="w-10/12 flex flex-col mx-auto">
+//       <h2 className="text-center font-bold text-2xl">Doctor List</h2>
+//       {status === 'loading' && <div>Loading...</div>}
+//       {status === 'failed' && <div>Error: {error}</div>}
+//       <div className="flex">
+//         {doctors.map((doctor) => (
+//           <div key={doctor.id}>
+//             <Link to={`/doctors/${doctor.id}`}>
+//               <div className="bg-white p-4 shadow-md rounded-full">
+//                 <p className="text-lg font-semibold mb-2">
+//                   {doctor.name} - {doctor.specialty}
+//                 </p>
+//                 <img
+//                   src={doctor.picture}
+//                   alt={doctor.name}
+//                   className="rounded-full h-9/12 w-9/12"
+//                 />
+//               </div>
+//             </Link>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default DoctorList;
+
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import {
-  fetchDoctors, selectDoctors, selectDoctorsStatus, selectDoctorsError,
+  fetchDoctors,
+  selectDoctors,
+  selectDoctorsStatus,
+  selectDoctorsError,
 } from '../../redux/doctorsSlice';
 
 function DoctorList() {
@@ -18,24 +72,23 @@ function DoctorList() {
     <div className="w-10/12 flex flex-col mx-auto">
       <h2 className="text-center font-bold text-2xl">Doctor List</h2>
       {status === 'loading' && <div>Loading...</div>}
-      {status === 'failed' && (
-      <div>
-        Error:
-        {error}
-      </div>
-      )}
-      <div className="flex ">
+      {status === 'failed' && <div>Error: {error}</div>}
+      <div className="flex">
         {doctors.map((doctor) => (
           <div key={doctor.id}>
-            <div className="bg-white p-4 shadow-md rounded-full">
-              <p className="text-lg font-semibold mb-2">
-                {doctor.name}
-                {' '}
-                -
-                {doctor.specialty}
-              </p>
-              <img src={doctor.picture} alt={doctor.name} className="rounded-full h-9/12 w-9/12" />
-            </div>
+            {/* Wrap each doctor card in a Link */}
+            <Link to={`/doctors/${doctor.id}`}>
+              <div className="bg-white p-4 shadow-md rounded-full">
+                <p className="text-lg font-semibold mb-2">
+                  {doctor.name} - {doctor.specialty}
+                </p>
+                <img
+                  src={doctor.picture}
+                  alt={doctor.name}
+                  className="rounded-full h-9/12 w-9/12"
+                />
+              </div>
+            </Link>
           </div>
         ))}
       </div>
