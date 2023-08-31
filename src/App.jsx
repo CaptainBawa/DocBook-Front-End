ec7434ceddd81c3383ad9b60a77ff4577b9466af
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-31 15:23:28 +0100

    import doctor component into app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Login from './components/Login';
 import Signup from './components/Signin';
+import AddDoctor from './components/AddDoctor';
 
 const App = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () => (
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/home" element={<Layout />} />
+      <Route path="/add-doctor" element={<AddDoctor />} />
     </Routes>
   </BrowserRouter>
 );
