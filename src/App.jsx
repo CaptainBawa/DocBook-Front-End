e7805e0f4b533fa5bbc95e5b1754c15c215e78d1
tomdan-ai <tomudoh258@gmail.com>
2023-08-31 17:27:55 +0100

    Add appointments and doctor detail's routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Login from './components/Login';
 import Signup from './components/Signin';
+import DoctorDetails from './components/doctors/DoctorDetails';
+import Appointments from './components/appointments/Appointments';
 
 const App = () => (
   <BrowserRouter>
@@ -10,6 +12,8 @@ const App = () => (
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/home" element={<Layout />} />
+      <Route path="/my-appointments" element={< Appointments/>} />
+      <Route path="/doctors/:doctorId" element={<DoctorDetails />} />
     </Routes>
   </BrowserRouter>
 );
