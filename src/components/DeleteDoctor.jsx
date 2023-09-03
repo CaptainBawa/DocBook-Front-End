4ac1b50628c0af91fe5f892565a45f5595366ec4
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-09-03 13:12:22 +0100

    Improve the UI/UX design

--- src/components/DeleteDoctor.jsx
+++ src/components/DeleteDoctor.jsx
@@ -21,40 +21,47 @@ const DeleteDoctor = () => {
   };
 
   return (
-    <div className="w-10/12 flex flex-col mx-auto">
-      <h2 className="text-center font-bold text-2xl">Doctor List</h2>
-      {status === 'loading' && <div>Loading...</div>}
-      {status === 'failed' && (
-        <div>
-          Error:
-          {error}
-        </div>
-      )}
-      <table className="table-auto w-full">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Doctor Name</th>
-            <th className="px-4 py-2">Actions </th>
-          </tr>
-        </thead>
-        <tbody>
-          {doctors.map((doctor) => (
-            <tr key={doctor.id}>
-              <td className="border px-4 py-2">{doctor.name}</td>
-              <td className="border px-4 py-2">
-                <button
-                  type="submit"
-                  className="bg-red-500 text-white px-2 py-1 rounded"
-                  onClick={() => handleDelete(doctor.id)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="w-full md:w-10/12 mx-auto">
+  <h2 className="text-center font-bold text-3xl mt-6 mb-8 text-indigo-700">Doctor List</h2>
+  {status === 'loading' && (
+    <div className="text-center text-gray-600 animate-pulse">
+      Loading...
     </div>
+  )}
+  {status === 'failed' && (
+    <div className="text-center text-red-500">
+      Error: {error}
+    </div>
+  )}
+  <div className="overflow-x-auto">
+    <table className="table-auto w-full bg-white shadow-lg rounded-lg">
+      <thead>
+        <tr className="bg-indigo-700 text-white">
+          <th className="px-6 py-3 text-left">Doctor Name</th>
+          <th className="px-6 py-3 text-left">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {doctors.map((doctor) => (
+          <tr key={doctor.id} className="border-b hover:bg-gray-100">
+            <td className="px-6 py-4 text-gray-800">{doctor.name}</td>
+            <td className="px-6 py-4">
+              <button
+                type="button"
+                className="bg-red-500 text-white px-4 py-2 rounded-full hover:bg-red-600 transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none"
+                onClick={() => handleDelete(doctor.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+</div>
+
+  
   );
 };
 
