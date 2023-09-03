5b6ba35b3b26f5df2051eb74e71894b18cfaf5d6
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-09-03 23:24:34 +0100

    refactor doctor details

--- src/components/doctors/DoctorDetails.jsx
+++ src/components/doctors/DoctorDetails.jsx
@@ -51,69 +51,56 @@ function DoctorDetails() {
   return (
     <>
       <Navigation />
-      <div>
-        <div>
-          <div>
-            <img
-              src={doctor.picture}
-              alt={doctor.name}
-              className="doc-img"
-            />
-          </div>
-          <div className="detials-div">
-            <h2 className="lead">{doctor.name}</h2>
-            <p className="pop1">
-              {' '}
-              -
-              {doctor.specialty}
-            </p>
-            <p className="pop  popa">
-              <strong>Email: &nbsp;  </strong>
-              {doctor.email}
-            </p>
-            <p className="pop">
-              <strong>Phone: &nbsp;  </strong>
-              {doctor.phone_number}
-            </p>
-            <p className="pop popb">
-              <strong>Address: &nbsp;  </strong>
-              {doctor.addresses[0].street}
-            </p>
-            <p className="pop">
-              <strong>City: &nbsp;  </strong>
-              {doctor.addresses[0].city}
-            </p>
-            <p className="pop popb">
-              <strong>State: &nbsp;  </strong>
-              {doctor.addresses[0].state}
-            </p>
-            <p className="pop">
-              <strong>Country: &nbsp;  </strong>
-              {doctor.addresses[0].country}
-            </p>
-            <p className="pop popc">
-              <strong>Price: &nbsp;  </strong>
-              {' '}
-              $
-              {doctor.price}
-            </p>
-          </div>
-          <button
-            type="button"
-            onClick={handleAppointment}
-            className="pop-btn mt-4 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md"
-          >
-            Reserve Appointment
-          </button>
-          <button
-            type="button"
-            className="popi-btn mt-4 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md"
-          >
-            <NavLink to="/home">&larr;</NavLink>
-          </button>
-
+      <div className="bg-gray-100 py-8">
+  <div className="container mx-auto px-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+      <div className="text-center md:text-left">
+        <img
+          src={doctor.picture}
+          alt={doctor.name}
+          className="w-48 h-48 md:w-64 md:h-64 object-cover rounded-full mx-auto md:mx-0"
+        />
+        <h2 className="text-3xl font-semibold text-gray-800 mt-6">
+          {doctor.name}
+        </h2>
+        <p className="text-xl text-gray-600 mb-2">
+          {doctor.specialty}
+        </p>
+        <div className="text-gray-800">
+          <p className="mb-2">
+            <strong>Email:</strong> {doctor.email}
+          </p>
+          <p className="mb-2">
+            <strong>Phone:</strong> {doctor.phone_number}
+          </p>
+          <p className="mb-2">
+            <strong>Address:</strong> {doctor.addresses[0].street}, {doctor.addresses[0].city}, {doctor.addresses[0].state}, {doctor.addresses[0].country}
+          </p>
+          <p className="mb-2">
+            <strong>Price:</strong> ${doctor.price}
+          </p>
         </div>
+        <button
+          type="button"
+          onClick={handleAppointment}
+          className="mt-4 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md transition duration-300 ease-in-out"
+        >
+          Reserve Appointment
+        </button>
       </div>
+      <div className="hidden md:block">
+        <button
+          type="button"
+          className="bg-green-500 hover:bg-green-600 text-white py-2 px-6 rounded-md transition duration-300 ease-in-out"
+        >
+          <NavLink to="/home">&larr; Go Back</NavLink>
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
     </>
   );
 }
