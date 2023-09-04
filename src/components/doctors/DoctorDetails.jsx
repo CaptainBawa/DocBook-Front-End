93ae47dcb8c9e8654a190aad56fa0c5ebb93c9ac
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-09-04 11:34:42 +0100

    fix navbar issue

--- src/components/doctors/DoctorDetails.jsx
+++ src/components/doctors/DoctorDetails.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink, useParams } from 'react-router-dom';
 
-import Navigation from '../Navigation';
 import {
   selectDoctors,
   selectDoctorsStatus,
@@ -44,64 +43,81 @@ function DoctorDetails() {
     );
   }
 
+  // checking if doctor exist
   if (!doctor) {
-    return <div>Doctor not found</div>;
+    return <div className="hidden">Doctor not found</div>;
   }
 
   return (
-    <>
-      <Navigation />
-      <div className="bg-gray-100 py-8">
-  <div className="container mx-auto px-4">
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-      <div className="text-center md:text-left">
-        <img
-          src={doctor.picture}
-          alt={doctor.name}
-          className="w-48 h-48 md:w-64 md:h-64 object-cover rounded-full mx-auto md:mx-0"
-        />
-        <h2 className="text-3xl font-semibold text-gray-800 mt-6">
-          {doctor.name}
-        </h2>
-        <p className="text-xl text-gray-600 mb-2">
-          {doctor.specialty}
-        </p>
-        <div className="text-gray-800">
-          <p className="mb-2">
-            <strong>Email:</strong> {doctor.email}
-          </p>
-          <p className="mb-2">
-            <strong>Phone:</strong> {doctor.phone_number}
-          </p>
-          <p className="mb-2">
-            <strong>Address:</strong> {doctor.addresses[0].street}, {doctor.addresses[0].city}, {doctor.addresses[0].state}, {doctor.addresses[0].country}
-          </p>
-          <p className="mb-2">
-            <strong>Price:</strong> ${doctor.price}
-          </p>
+
+    <div className="bg-gray-100 py-8">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+          <div className="text-center md:text-left ">
+            <img
+              src={doctor.picture}
+              alt={doctor.name}
+              className="w-48 h-48 md:w-64 md:h-64 object-cover rounded-full mx-auto md:mx-0"
+            />
+            <h2 className="text-3xl font-semibold text-gray-800 mt-6">
+              {doctor.name}
+            </h2>
+            <p className="text-xl text-gray-600 mb-2">
+              {doctor.specialty}
+            </p>
+            <div className="text-gray-800">
+              <p className="mb-2">
+                <strong>Email:</strong>
+                {' '}
+                {doctor.email}
+              </p>
+              <p className="mb-2">
+                <strong>Phone:</strong>
+                {' '}
+                {doctor.phone_number}
+              </p>
+              <p className="mb-2">
+                <strong>Address:</strong>
+                {' '}
+                {doctor.addresses[0].street}
+                ,
+                {' '}
+                {doctor.addresses[0].city}
+                ,
+                {' '}
+                {doctor.addresses[0].state}
+                ,
+                {' '}
+                {doctor.addresses[0].country}
+              </p>
+              <p className="mb-2">
+                <strong>Price:</strong>
+                {' '}
+                $
+                {doctor.price}
+              </p>
+            </div>
+            <button
+              type="button"
+              onClick={handleAppointment}
+              className="mt-4  bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md transition duration-300 ease-in-out"
+            >
+              Reserve Appointment
+            </button>
+            <button
+              type="button"
+              className="bg-green-500 hover:bg-green-600 text-white py-2 px-6 ml-4 rounded-md transition duration-300 ease-in-out"
+            >
+              <NavLink to="/home">&larr; Go Back</NavLink>
+            </button>
+
+          </div>
+
         </div>
-        <button
-          type="button"
-          onClick={handleAppointment}
-          className="mt-4 bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md transition duration-300 ease-in-out"
-        >
-          Reserve Appointment
-        </button>
-      </div>
-      <div className="hidden md:block">
-        <button
-          type="button"
-          className="bg-green-500 hover:bg-green-600 text-white py-2 px-6 rounded-md transition duration-300 ease-in-out"
-        >
-          <NavLink to="/home">&larr; Go Back</NavLink>
-        </button>
+
       </div>
     </div>
-  </div>
-</div>
 
-
-    </>
   );
 }
 
