0a668be08b0d036d13543c4333698676006304a1
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-09-06 17:24:24 +0100

    change the xs font-size

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'xs': '0.55rem', 
+      },
+    },
   },
   plugins: [],
 };
