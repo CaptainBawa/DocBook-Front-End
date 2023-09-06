ccd30626f6f1bcb3cd7b1a9ff61a56855ce20a3f
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-09-06 16:36:29 +0100

    Fix responsive issues

--- src/components/appointments/Appointments.jsx
+++ src/components/appointments/Appointments.jsx
@@ -42,32 +42,32 @@ const Appointments = () => {
       </div>
     )}
     <div className="overflow-x-auto">
-      <table className="table-auto w-full md:w-3/4 sm:w-11/12">
+      <table className="table-auto w-full sm:w-11/12 md:w-3/4 lg:w-4/5">
         <thead>
           <tr>
-            <th className="text-sm sm:text-base">Date</th>
-            <th className="text-sm sm:text-base">Patient's name</th>
-            <th className="text-sm sm:text-base">Doctor's name</th>
-            <th className="text-sm sm:text-base">Fees($)</th>
-            <th className="text-sm sm:text-base">Doctor's contact</th>
-            <th className="text-sm sm:text-base">Appointments Location</th>
-            <th className="text-sm sm:text-base">Actions</th>
+            <th className="text-xs sm:text-base md:text-sm lg:text-base">Date</th>
+            <th className="text-xs sm:text-base md:text-sm lg:text-base">Patient's name</th>
+            <th className="text-xs sm:text-base md:text-sm lg:text-base">Doctor's name</th>
+            <th className="text-xs sm:text-base md:text-sm lg:text-base">Fees($)</th>
+            <th className="text-xs sm:text-base md:text-sm lg:text-base">Doctor's contact</th>
+            <th className="text-xs sm:text-base md:text-sm lg:text-base">Appointments Location</th>
+            <th className="text-xs sm:text-base md:text-sm lg:text-base">Actions</th>
           </tr>
         </thead>
         <tbody>
           {appointments.map((appointment) => (
             <tr key={appointment.id}>
-              <td className="border py-2 text-sm sm:text-base">{appointment.appointment_date}</td>
-              <td className="border py-2 text-sm sm:text-base">{appointment.user.username}</td>
-              <td className="border py-2 text-sm sm:text-base">{appointment.doctor.name}</td>
-              <td className="border py-2 text-sm sm:text-base">{appointment.doctor.price}</td>
-              <td className="border py-2 text-sm sm:text-base">{appointment.doctor.email}</td>
-              <td className="border py-2 text-sm sm:text-base">{appointment.city}</td>
-              <td className="border py-2">
+              <td className="border py-2 text-xs sm:text-base md:text-sm lg:text-base">{appointment.appointment_date}</td>
+              <td className="border py-2 text-xs sm:text-base md:text-sm lg:text-base">{appointment.user.username}</td>
+              <td className="border py-2 text-xs sm:text-base md:text-sm lg:text-base">{appointment.doctor.name}</td>
+              <td className="border py-2 text-xs sm:text-base md:text-sm lg:text-base">{appointment.doctor.price}</td>
+              <td className="border py-2 text-xs sm:text-base md:text-sm lg:text-base">{appointment.doctor.email}</td>
+              <td className="border py-2 text-xs sm:text-base md:text-sm lg:text-base">{appointment.city}</td>
+              <td className="">
                 <button
                   type="button"
                   onClick={() => handleDeleteAppointment(appointment.id)}
-                  className="bg-red-500 hover:bg-green-700 text-white font-bold py-1 px-2 md:py-2 md:px-4 rounded-full cancel-app"
+                  className="bg-red-500 hover:bg-green-700 text-white font-bold py-1 px-2 sm:py-1 sm:px-1  md:py-2 md:px-2 lg:py-2 lg:px-4 rounded-full cancel-app"
                 >
                   Cancel
                 </button>
@@ -79,6 +79,7 @@ const Appointments = () => {
     </div>
   </div>
 </div>
+
 
   );
 };
